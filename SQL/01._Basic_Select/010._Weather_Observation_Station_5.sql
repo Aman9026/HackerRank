77e9499a5fcc7ce96074e9fe07e51abffbeb7ca1
Aman Gupta <amangupta884036@gmail.com>
2020-03-07 10:32:22 +0530

    Added 010._Weather_Observation_Station_5.sql

---
+++ SQL/01._Basic_Select/010._Weather_Observation_Station_5.sql
@@ -0,0 +1,17 @@
+-- # Problem: https://www.hackerrank.com/challenges/weather-observation-station-5/problem
+-- # Score: 30
+
+
+SELECT *
+FROM
+    (SELECT CITY, LENGTH(CITY)
+    FROM STATION
+    ORDER BY LENGTH(CITY), CITY)
+WHERE ROWNUM = 1
+UNION
+SELECT *
+FROM
+    (SELECT CITY, LENGTH(CITY)
+    FROM STATION
+    ORDER BY LENGTH(CITY) DESC, CITY)
+WHERE ROWNUM = 1;
