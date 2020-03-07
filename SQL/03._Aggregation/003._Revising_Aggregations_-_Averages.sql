d63fe1bdef85b18bbf7eeb833ddc65d6c498ccaa
Aman Gupta <amangupta884036@gmail.com>
2020-03-07 13:23:51 +0530

    Added 003._Revising_Aggregations_-_Averages.sql

---
+++ SQL/03._Aggregation/003._Revising_Aggregations_-_Averages.sql
@@ -0,0 +1,7 @@
+-- # Problem: https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem
+-- # Score: 10
+
+
+SELECT AVG(Population)
+FROM City
+WHERE District = 'California';
