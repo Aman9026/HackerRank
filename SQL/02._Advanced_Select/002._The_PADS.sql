7c31efbc47f849e6907cfa00a02d51360de5d18b
Aman Gupta <amangupta884036@gmail.com>
2020-03-13 20:16:50 +0530

    Updated 002._The_PADS.sql

--- SQL/02._Advanced_Select/002._The_PADS.sql
+++ SQL/02._Advanced_Select/002._The_PADS.sql
@@ -10,3 +10,6 @@ SELECT 'There are a total of ' || Count(Name) || ' ' || LOWER(Occupation) || 's.
 FROM Occupations
 GROUP BY Occupation
 ORDER BY Count(Name), Occupation;
+ 
+ 
+ /*Contributed by AMAN KUMAR GUPTA*/
